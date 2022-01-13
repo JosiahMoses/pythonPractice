000f67f5e76a15ca49a749a274dd1704df88cb76
JosiahMoses <josiahmoses117@gmail.com>
2022-01-13 15:13:40 -0600

    Added Basic Programs
    Added Hello World and List programs.

---
+++ Basics/lists.py
@@ -0,0 +1,8 @@
+cars = ['camaro', 'corvette', 'challenger', 'charger']
+print(cars)
+print(cars[1])
+print(cars[2].title)
+print(cars[-1])
+
+message = f"My dream car is a {cars[1]}."
+print(message)
